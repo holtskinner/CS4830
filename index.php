c5e76d72ffce86fc2c8a3e24fc9887fb90d68361
Holt Skinner <HoltWasHere@gmail.com>
2016-10-05 15:26:22 -0500

    Finally got some stuff to work!
    use index.php/Teams in path

--- index.php
+++ index.php
@@ -1,14 +1,70 @@
 <?php
 require 'flight/Flight.php';
+//
+// class Team {
+//   public $City;
+//   public $Name;
+//   public $HomeStadium;
+//   public $Players = [];
+//
+//   function Team($City, $Name, $HomeStadium) {
+//     $this->$City = $City;
+//     $this->$Name = $Name;
+//     $this->$HomeStadium = $HomeStadium;
+//     array_push($this->$Players, $Player);
+//   }
+//
+// }
+//
+// class Player {
+//   var $FirstName;
+//   var $LastName;
+//   var $Age;
+//   var $Salary;
+//
+//   function Player($FirstName, $LastName, $Age, $Salary) {
+//     $this->$FirstName = $FirstName;
+//     $this->$LastName = $LastName;
+//     $this->$Age = $Age;
+//     $this->$Salary = $Salary;
+//   }
+// }
+//
+// class Stadium {
+//   var $Name;
+//   var $Capacity;
+//   var $TicketPrice;
+//
+//   function Stadium($Name, $Capacity, $TicketPrice) {
+//     $this->$Name = $Name;
+//     $this->$Capacity = $Capacity;
+//     $this->$TicketPrice = $TicketPrice;
+//   }
+// }
 
-Flight::route('/index.php', function(){
-    echo 'Hello world!';
+Flight::route('DELETE /index.php/Teams', function(){
+  if (isset($Teams)) {
+    unset($Teams);
+  }
 });
 
-Flight::route('/index.php/hey', function(){
-    echo 'Hey world!';
+Flight::route('GET /index.php/Teams', function() {
+  echo "Get";
 });
 
+Flight::route('POST /Teams', function() {
+  //Create New array if one doesn't exist
+  //JSON data
+  //$id = Flight::request()->data->id;
+  if(!isset($Teams)) {
+    $Teams = [];
+  }
+
+  array_push($Teams, )
+  echo 'Post';
+});
 
 Flight::start();
+
+echo 'hello';
 ?>
