417e0d45d2966df8dc3182eef08956c5947002c4
Holt Skinner <HoltWasHere@gmail.com>
2016-10-06 22:15:38 -0500

    Added More Database Studd
    Currently compiles, but doesn’t run

--- Challenge 2/index.php
+++ Challenge 2/index.php
@@ -1,7 +1,10 @@
 <?php
+// ini_set('display_errors','On');
+// error_reporting(E_ALL | E_STRICT);
+
 require 'flight/Flight.php';
 //Using Flight PHP framework flightphp.com for REST
-require  'medoo.php';
+require 'medoo.php';
 //Using Medoo.php framework with MySQL for database interactions
 
 //Create link to database
@@ -9,26 +12,30 @@ $database = new medoo([
 	'database_type' => 'mysql',
 	'database_name' => 'challenge2',
 	'server' => 'localhost',
-	'username' => '',
+	'username' => 'root',
 	'password' => '1234',
-	'charset' => 'utf8',
+	'charset' => 'utf8'
 ]);
 
 //DELETE ALL Teams
-Flight::route('DELETE /index.php/Teams', function(){
-  $database->delete("Teams", "*");
+Flight::route('DELETE /index.php/Teams', function($database) {
+  $database->delete("team", "*");
 });
 
 Flight::route('POST /index.php/Teams', function() {
+
   //JSON data from body
   $json = Flight::request()->data;
+  var_dump($json);
   //Separate stadium sub-object out for simplicity
   $stadium = $json["stadium"];
+
   //INSERT Team and stadium into database
   $database->insert("team",[
     "name" => $json->Name,
     "city" => $json->City
   ]);
+
   $database->insert("stadium",[
     "name" => $stadium["Name"],
     "capacity" => $stadium["Capacity"],
@@ -36,13 +43,13 @@ Flight::route('POST /index.php/Teams', function() {
   ]);
 });
 
-Flight::route('GET /index.php/Teams', function() {
+Flight::route('GET /index.php/Teams', function($database) {
   echo "Get";
 });
 
-Flight::route('PUT /index.php/Teams'), function(){
+Flight::route('PUT /index.php/Teams', function($database) {
   echo "PUT";
-}
+});
 
 Flight::start();
 ?>
