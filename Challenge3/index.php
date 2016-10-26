a6ce9d1b8cf141eea0d138e29dbef732205fc92a
Holt Skinner <HoltWasHere@gmail.com>
2016-10-25 20:04:01 -0500

    Added Materialize

--- Challenge3/index.php
+++ Challenge3/index.php
@@ -1,2 +1,39 @@
-<?php
- ?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link type="text/css" rel="stylesheet" href="css/materialize.min.css" media="screen,projection" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" /> </head>
+
+<body>
+    <div class="container">
+        <h1>Challenge 3<h1>
+    <h6>Page 1</h6>
+    <div class="row">
+        <form class="col s12">
+            <div class="row">
+                <div class="input-field col s6"> <i class="material-icons prefix">account_circle</i>
+                    <input id="icon_prefix" type="text" class="validate">
+                    <label for="icon_prefix">First Name</label>
+                </div>
+                <div class="input-field col s6"> <i class="material-icons prefix">account_circle</i>
+                    <input id="icon_prefix" type="text" class="validate">
+                    <label for="icon_prefix">Last Name</label>
+                </div>
+                <div class="input-field col s6"> <i class="material-icons prefix">phone</i>
+                    <input id="icon_telephone" type="tel" class="validate">
+                    <label for="icon_telephone">Telephone</label>
+                </div>
+            </div>
+            <button class="btn waves-effect waves-light" type="submit" name="action">Submit
+   <i class="material-icons right">send</i>
+ </button>
+        </form>
+    </div>
+        </div>
+    <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script type="text/javascript" src="js/materialize.min.js"></script>
+</body>
+
+</html>
